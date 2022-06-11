be4a5d035bee3fa26e9a82fd7454dbc39cce7b80
yawa107 <100961922+yawa107@users.noreply.github.com>
2022-06-11 12:24:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,10 +21,10 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 //aumto functioner
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
-global.autoReadGc = true //auto reading in gc (true to on, false to off)
+global.autoReadGc = false //auto reading in gc (true to on, false to off)
 global.autoReadAll = false // auto reading in all pchat gc and status (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
-global.available = true //auto available (true to on, false to off)
+global.available = false //auto available (true to on, false to off)
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemsthumb.jpg") //ur thumb pic
